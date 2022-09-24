22cc5d9749572c3f6e545c77eaaa73c5619b4d4f
Ekai Zendō <ekai@tuta.io>
2022-09-24 16:28:47 +0100

    reset to base application && project configs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,25 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Zed Kaidō (Artist, DJ, Producer, Coder)</title>
+	<meta name="description" content="@zedkaido with music that makes your transcend this existance and jump into a state of dance. " />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<a>the start is here</a>
 </section>
 
 <style>
 	section {
+		height: 100vh;
+		width: 100%;
+		background-color: black;
+		margin:0;
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+		flex-direction: row;
+		justify-content: start;
+		align-items: start;
 		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
