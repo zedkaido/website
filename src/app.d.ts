45a9e02629451f6c542d6f4bc561287feda5735f
Zed Kaido <ekai@tuta.io>
2022-10-01 20:01:31 +0100

    add adapter-cloudflare (for deployment)

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,9 +6,17 @@ declare namespace App {
 		userid: string;
 	}
 
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache }
+	}
+
 	// interface PageData {}
 
 	// interface Error {}
-
-	// interface Platform {}
 }
