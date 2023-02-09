40dd1ac310f6b75de0cae500166c8ec5f9b631d4
Zed Kaido <ekai@tuta.io>
2023-02-09 01:47:09 +0000

    update blog post

--- src/routes/blog/second-rendez-vous-redux/+page.svelte
+++ src/routes/blog/second-rendez-vous-redux/+page.svelte
@@ -27,14 +27,9 @@
             As a long-time fan of the track, I wanted to put my own spin on it, and bring it to a new audience. I wanted to create a modern, upbeat danceable twist, packed with soaring synths, punchy bass, and driving beats. My vision was to create a track that would transport listeners to another dimension, a journey through space and sound.
         </p>
 
-        <h3>Bringing an Amazing Melody to the Big Festivals and Clubs</h3>
-        <p>
-            Music production has come a long way since the release of the original track, and I had access to a wide range of tools and technology that wasn't available back in the 80s. I wanted to take advantage of these advancements and create a modern interpretation of the track that would stand out from the crowd. I wanted to bring an amazing melody to the big festivals and clubs, and make it stand out from the often repetitive and uninteresting tracks that dominate the charts today.
-        </p>
-
         <h3>The Process of Creating "Second Rendez-Vous (Redux)"</h3>
         <p>
-            The process of creating "Second Rendez-Vous (Redux)" was a challenging and rewarding experience. I spent countless hours in the studio, working on every detail, and experimenting with different sounds and effects. The final track is 7:27 minutes long, divided into seventeen sections, each with its own story and emotion - I could tell you about each one of them, but I believe it comes to you to define the meaning and the story of the song:
+            Challenging, but rewarding. The final track is 7:27 minutes long, divided into seventeen sections, each with its own story and emotion - I could tell you about each one of them, but I believe it comes to you to define the meaning and the story of the song:
         </p>
 
         <iframe title="Second Rendez-Vous (Redux) - Bandcamp Embed" style="border: 0; width: 100%; height: 442px;" src="https://bandcamp.com/EmbeddedPlayer/track=3682126709/size=large/bgcol=ffffff/linkcol=101010/tracklist=false/transparent=true/" seamless SameSite=Strict><a href="https://zedkaido.bandcamp.com/track/second-rendez-vous-redux">Second Rendez-Vous (Redux) by Zed Kaido</a></iframe>
@@ -54,15 +49,7 @@
 
         <h3>A Complex and Intricate Composition</h3>
         <p>
-            The soaring synths, punchy bass, driving beats and the risers and fallers create a sense of movement, tension and propulsion that's impossible to resist. Moreover, the track features a variety of electronic sounds and effects that are woven together to create a rich and dynamic soundscape.
-        </p>
-
-        <p>
-            The track is designed to take the listener on a journey through different emotions and moods, each section of the track has its own story and feeling, and it seamlessly flows from one section to another. The result is a track that's not only danceable but also emotionally engaging and thought-provoking.
-        </p>
-
-        <p>
-            The track features 66 layers, making it a truly intricate and immersive composition. Each layer adds something unique to the track, and it's designed to be listened to multiple times, revealing new elements and details with each listen. The track is not only a feast for the ears but also for the mind, it's a track that I believe will keep you engaged and interested for a long time.
+            The soaring synths, punchy bass, driving beats and the tension and propulsion that's simply impossible to resist - a rich and dynamic soundscape. Thought-provoking at best, enjoyable at least ¯\_(ツ)_/¯
         </p>
 
         <!-- <iframe src="https://youtu.be/MJ5nhiqgyLU"> -->
