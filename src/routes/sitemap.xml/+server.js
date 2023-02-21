b3cffd11a6410584d4e6a62c587496d34dedd991
Zed Kaido <ekai@tuta.io>
2023-02-21 11:49:16 +0000

    update sitemap.xml

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -9,20 +9,11 @@ export async function GET({ setHeaders }) {
         'Content-Type': 'application/xml'
     })
     const website="https://zedkaido.com"
-    const xml = `<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-      xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
-      xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-      xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
-      xmlns:pagemap="http://www.google.com/schemas/sitemap-pagemap/1.0"
-      xmlns:xhtml="http://www.w3.org/1999/xhtml">
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>${website}</loc>
-        <lastmod>2023-01-28</lastmod>
+        <lastmod>2023-02-21</lastmod>
       </url>
       <url>
         <loc>${website}/music</loc>
@@ -34,11 +25,19 @@ export async function GET({ setHeaders }) {
       </url>
       <url>
         <loc>${website}/blog</loc>
-        <lastmod>2023-01-28</lastmod>
+        <lastmod>2023-02-21</lastmod>
       </url>
       <url>
         <loc>${website}/socials</loc>
-        <lastmod>2023-02-13</lastmod>
+        <lastmod>2023-02-21</lastmod>
+      </url>
+      <url>
+        <loc>${website}/blog/categories/internet</loc>
+        <lastmod>2023-02-21</lastmod>
+      </url>
+      <url>
+        <loc>${website}/blog/categories/music-releases</loc>
+        <lastmod>2023-02-21</lastmod>
       </url>
       ${posts
         .map(
@@ -50,5 +49,5 @@ export async function GET({ setHeaders }) {
         .join('')}
     </urlset>`
 
-    return new Response(xml)
+  return new Response(xml);
 }
