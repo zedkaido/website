db56b7c8f2f2af8486bd05e9e53c1807792064f9
Zed Kaido <ekai@tuta.io>
2023-07-15 00:41:05 +0100

    add layout base

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '$lib/styles/global.css';
+</script>
+
+<div class="page">
+	<p>art by</p>
+	<h1>zed kaido</h1>
+	<p>Life is a symphony. Turn up the volume!</p>
+	<a href="blog"> blog </a>
+	<a href="music"> music </a>
+	<a href="music"> dj-shows </a>
+	<a href="merch"> merch </a>
+	<a href="Links"> contact </a>
+	<slot />
+</div>
+
+<style>
+	.page {
+		height: 100%;
+		box-sizing: border-box;
+		background-color: var(--color-2);
+		padding: 10%;
+	}
+
+	h1 {
+		margin: 0;
+	}
+</style>
